7060dc150e0b3e8c6905a0b2990439abc4c84ac6
helen bergari <helenbergari@hotmail.com>
2026-05-09 15:04:59 +0200

    Add project files.

---
+++ TeamManagementWebApi/Controllers/TeamMembersController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TeamManagementWebApi.Models;
+
+namespace TeamManagementWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TeamMembersController : ControllerBase
+    {
+        private static List<TeamMember> teamMembers = new List<TeamMember>
+        {
+            new TeamMember
+            {
+                Id = 1,
+                Name = "Samantha William",
+                Email = "samantha@gmail.com",
+                Role = "Student"
+            },
+            new TeamMember
+            {
+                Id = 2,
+                Name = "Adam Smith",
+                Email = "adamsmith@gmail.com",
+                Role = "Student"
+            }
+        };
+
+        [HttpGet]
+        public IActionResult GetTeamMembers()
+        {
+            return Ok(teamMembers);
+        }
+
+        [HttpPost]
+        public IActionResult AddTeamMember(TeamMember newMember)
+        {
+            teamMembers.Add(newMember);
+
+            return Ok(newMember);
+        }
+    }
+}
---
+++ TeamManagementWebApi/Models/TeamMember.cs
@@ -0,0 +1,13 @@
+﻿namespace TeamManagementWebApi.Models
+{
+    public class TeamMember
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public string Email { get; set; } = null!;
+
+        public string Role { get; set; } = null!;
+    }
+}
